3108dfe205ef474efaa3e5bd13b065b90948d050
acorales <abel.corales@gmail.com>
2022-04-25 12:55:39 -0500

    change instance type

--- mod-proj-ec2-V2/main.tf
+++ mod-proj-ec2-V2/main.tf
@@ -1,6 +1,16 @@
+
+terraform {
+  backend "s3" {
+    bucket = "acorales-terraform"
+    key    = "infra/mod-proj-ec2-V2.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
     region = "us-east-1"
 }
+
 
 module "vpc" {
     source = "./vpc"
@@ -11,35 +21,33 @@ module "vpc" {
 
 module "ec2"  {
     source = "./ec2"
-    ec2type = "t3.micro"
+    ec2type = "t3.small"
     ec2iface = module.vpc.ec2_network_interface
     ec2name = "instance1"
     user_data = <<-EOF
 	        #!/bin/bash
-		    echo "Hola mundo cruel"
+            cd /home/ec2-user
+		    echo "Hola mundo cruel" > hola.txt
 		    EOF
     
 }
 
-module "ec2-2"    {
+module "ec2"  {
     source = "./ec2"
-    ec2type = "t2.micro"
-    ec2iface = module.vpc.ec2_network_interface-2
-    ec2name = "instance2"
+    ec2type = "t3.small"
+    ec2iface = module.vpc.ec2_network_interface
+    ec2name = "instance1"
     user_data = <<-EOF
 	        #!/bin/bash
-		    sudo yum update -y
-		    sudo yum -y install httpd -y
-		    sudo service httpd start
-		    echo "Hola mundo cruel C2 V2 $(hostname -f)" > /var/www/html/index.html
+            cd /home/ec2-user
+		    echo "Hola mundo cruel" > hola.txt
 		    EOF
     
-   
 }
 
 module "ec2-3"    {
     source = "./ec2"
-    ec2type = "t2.micro"    
+    ec2type = "t3.micro"    
     ec2iface = module.vpc.ec2_network_interface-3
     ec2name = "instance3"
     user_data = <<-EOF
